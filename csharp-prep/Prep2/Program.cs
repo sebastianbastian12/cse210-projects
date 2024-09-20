769d94dea92b2cfd10a22a51426e692ad9d1b5a4
Se-Bastian12 <sebastian.oliveros12@gmail.com>
2024-09-20 16:09:42 -0500

    second assignment first week of the semester

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade percentage, please.");
+        string percentage = Console.ReadLine();
+        int gradePercentage = int.Parse(percentage);
+
+        string grade = "";
+
+        if (gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercentage >=80)
+        {
+            grade = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            grade = "D";
+        }
+
+        else {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade for this semerster is {grade}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the semester");
+        }
+        else {
+            Console.WriteLine("You need to improve to passed the semester");
+        }
     }
 }
