eb7bd5f82468c412e92f4a9bea4bba34c022344e
Se-Bastian12 <sebastian.oliveros12@gmail.com>
2024-09-21 07:48:05 -0500

    third assignment first week of the semester

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1,101);
+        Console.WriteLine($"The magic is {magicNumber}");
+        int guessedNumber = 0;
+
+       do {
+            Console.WriteLine("What is the magic number?");
+            string Number = Console.ReadLine();
+            guessedNumber = int.Parse(Number);
+
+            if (guessedNumber == magicNumber)
+            {
+                Console.WriteLine("Great! You guessed.");
+            }
+            else if (guessedNumber > magicNumber)
+            {
+                Console.WriteLine("Lower than this.");
+            }
+            else
+            {
+                Console.WriteLine("Higher than this.");
+            }
+        } while(guessedNumber != magicNumber);    
     }
 }
